172f3573a45146b4e25fadfa8223eab591656595
Gavin <Gavinn8@nycstudents.net>
2023-09-21 09:10:30 -0400

    added added stats

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -5,12 +5,12 @@ public class TipCalculator {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Welcome to Tip Calculator!");
-
-        System.out.println("How many people are you dining with?: ");
+        System.out.print("How many people are you dining with?: ");
         int people = scan.nextInt();
-        System.out.println("What's the tip percentage? (0 - 100): ");
+        System.out.print("What's the tip percentage? (0 - 100): ");
         double tipPercentage = scan.nextDouble();
         System.out.println("------------------------");
+
         double itemCost = 0;
         double total = 0;
 
@@ -19,5 +19,16 @@ public class TipCalculator {
             itemCost = scan.nextDouble();
             total += itemCost;
         }
+        total ++;
+        double tip = (tipPercentage / 100.0) * total;
+        System.out.println("------------------------");
+        System.out.println("Total bill before tip: $" + total);
+        System.out.println("Total tip percentage: " + tipPercentage + "%");
+        System.out.println("Total tip: $" + (int)(tip * 100) / 100.0);
+        System.out.println("Total bill with tip: $" + (int)((total + tip) * 100) / 100.0);
+        System.out.println("Cost per person before tip: $" + (int)(total / people * 100) / 100.0);
+        System.out.println("Tip per person: $" + (int)(tip / people * 100) / 100.0);
+        System.out.println("Total cost per person: $" + (int)((tip + total) * 100) / 100.0);
+        System.out.println("------------------------");
     }
 }
