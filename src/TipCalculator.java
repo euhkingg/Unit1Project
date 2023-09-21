03c4322360c17379887f5aeae906f9390e1ec4a1
Gavin <Gavinn8@nycstudents.net>
2023-09-21 08:47:30 -0400

    created base program

---
+++ src/TipCalculator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+public class TipCalculator {
+        public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to Tip Calculator!");
+
+        System.out.println("How many people are you dining with?: ");
+        int people = scan.nextInt();
+        System.out.println("What's the tip percentage? (0 - 100): ");
+        double tipPercentage = scan.nextDouble();
+        System.out.println("------------------------");
+        double itemCost = 0;
+        double total = 0;
+
+        while (itemCost != -1) {
+            System.out.println("Enter an items cost (-1 to end) : ");
+            itemCost = scan.nextDouble();
+            total += itemCost;
+        }
+    }
+}
